1da71c4b6adda8dad3a5b989498c24664ea60737
Shareej <shareejvk@gmail.com>
2025-05-17 15:16:59 +0530

    asset folder chanegd

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -83,7 +83,7 @@ export const config: VendureConfig = {
             // For local dev, the correct value for assetUrlPrefix should
             // be guessed correctly, but for production it will usually need
             // to be set manually to match your production url.
-            assetUrlPrefix: IS_DEV ? undefined : 'https://www.my-shop.com/assets/',
+            assetUrlPrefix: IS_DEV ? undefined : 'https://vendure-backend-xvqg.onrender.com/assets/',
         }),
         DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
         DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
